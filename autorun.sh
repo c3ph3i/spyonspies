50c6fe2c5da1a00270206463f6b08df251d8a4e5
c3ph3i <c3ph3i@protonmail.com>
2018-03-12 23:16:21 +0400

    Init commit

---
+++ autorun.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+pid=$(pgrep sessio)
+
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/${pid}/environ|cut -d= -f2-)
+
+DIR=$(dirname $([ -L $0 ] && readlink -f $0 || echo $0))
+
+# First execute. Let you to know who turned on your comp
+python $DIR/spy.py
+
+# Handle unlocking
+
+dbus-monitor --session "type='signal',interface='org.gnome.ScreenSaver'" |
+  while read x; do
+    case "$x" in 
+      *"boolean false"*) python $DIR/spy.py;;  
+    esac
+  done
